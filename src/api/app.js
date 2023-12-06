54b578c9d8a26e715c747ab1c19c5e74353896d3
Argeliosk8 <argeli.baca@gmail.com>
2023-12-06 11:07:46 -0600

    added find all, find one and add one recruiter routes

--- src/api/app.js
+++ src/api/app.js
@@ -1,27 +1,38 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const { run } = require('./utils')
+const { findAllRecruiters, findByName, addRecruiter } = require('./utils')
 const app = express()
 const port = 5000
 
 app.use(bodyParser.json());
 
+/*
 app.param('status', (req, res, next, stat) => {
   const status = String(stat)
   req.status = status
 })
+*/
 
-app.get('/hello', async (req, res) => {
-    const query = {}
-    const recruiter = await run(query)
+app.get('/find/allrecruiters', async (req, res) => {
+    const recruiter = await findAllRecruiters()
     res.send(recruiter)
   })
   
-  app.get('/hello/:status', (req, res) => {
-    run()
+app.get('/find/recruiter', async (req, res) => {
+  const body = req.body
+  const name = body.name
+  const recruiter = await findByName(name)
+  res.send(recruiter)
+})
 
-  })
 
-  app.listen(port, () => {
+app.post('/add/recruiter', async (req, res) => {
+  const body = req.body
+  const recruiter = body.recruiter
+  const result = await addRecruiter(recruiter)
+  res.send(result)
+})
+
+app.listen(port, () => {
     console.log(`Express listening on port ${port}`)
   })
--- src/api/utils.js
+++ src/api/utils.js
@@ -22,3 +22,53 @@ exports.run = async (query) => {
     await client.close();
   }
 }
+
+
+exports.findAllRecruiters = async () => {
+    try {
+      const database = client.db('dat');
+      const recruiters = database.collection('recruiters');
+      const query = {}
+      const options = {
+        sort: {"name": 1},
+        projection: {_id: 0, name: 1, reqs: 1, status: 1}
+      }
+      const recruiter = await recruiters.find(query, options).toArray();
+      console.log(recruiter)
+      return recruiter
+    } catch (error) {
+      console.log(error)
+      await client.close();
+    }
+  }
+
+exports.findByName = async (name) => {
+    try {
+        const database = client.db('dat');
+        const recruiters = database.collection('recruiters');
+        const query = { name: name }
+        const options = {
+            projection: {_id: 0, name: 1, reqs: 1, status: 1}
+        }
+        const recruiter = await recruiters.findOne(query, options);
+        console.log(recruiter)
+        return recruiter
+      } catch (error) {
+        console.log(error)
+        await client.close();
+      }
+}
+
+exports.addRecruiter = async (recruiter) => {
+    try {
+        const database = client.db('dat');
+        const recruiters = database.collection('recruiters');
+        const result = await recruiters.insertOne(recruiter)
+        console.log(result)
+        return result
+
+    } catch (error) {
+        console.log(error)
+        await client.close();
+    }
+}
